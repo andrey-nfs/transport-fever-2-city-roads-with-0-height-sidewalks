2fad49352737a8938924fc47816820ba785fe902
Andrey Ignatyev <soulseeker-nf@yandex.ru>
2020-10-10 11:23:56 +1100

    Initial Commit

---
+++ mod.lua
@@ -0,0 +1,19 @@
+function data()
+return {
+  info = {
+    minorVersion = 0,
+    severityAdd = "NONE",
+    severityRemove = "NONE",
+    name = _("City roads with 0 height sidewalks"),
+    description = _("City roads with 0 height sidewalks"),
+	tags = { "street", "road"},
+	visible = true,
+    authors = {
+      {
+        name = "NFS Moscow Melbourne",
+        role = "Creator",	
+      },      
+    },    
+  },
+}
+end
